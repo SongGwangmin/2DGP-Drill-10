07a7b516ea8854b092d2e67203e9e319755d6736
Gwangmin Song <thdalsrhkd@naver.com>
2025-11-04 19:18:09 +0900

    boy에서 FRAMES_PER_SEC 가져오기

--- Lecture12_Time/bird.py
+++ Lecture12_Time/bird.py
@@ -3,15 +3,19 @@ import game_world
 import game_framework
 
 PIXEL_PER_METER = (10.0 / 0.3)
-GRAVITY = 9.8  # m/s^2
 
 
-class Ball:
+time_per_action = 1.2
+frames_per_action = 8
+actions_per_time = 1.0 / time_per_action
+FRAMES_PER_SEC = frames_per_action * actions_per_time
+
+class Bird:
     image = None
 
     def __init__(self, x = 400, y = 300, throwin_speed = 15, throwin_angle = 45): #velocity 15m/s
-        if Ball.image == None:
-            Ball.image = load_image('ball21x21.png')
+        if Bird.image == None:
+            Bird.image = load_image('ball21x21.png')
         self.x, self.y = x, y
         self.xv = throwin_speed * math.cos(math.radians(throwin_angle))
 
@@ -21,7 +25,6 @@ class Ball:
         self.image.draw(self.x, self.y)
 
     def update(self):
-        self.yv -= GRAVITY * game_framework.frame_time
 
         self.x += self.xv * game_framework.frame_time * PIXEL_PER_METER
         self.y += self.yv * game_framework.frame_time * PIXEL_PER_METER
